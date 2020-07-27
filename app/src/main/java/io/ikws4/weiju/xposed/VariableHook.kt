420a8ca84ed911b2821b4b4b67a110663258b1de
xing2387 <einstein2387@sina.cn>
2020-07-28 02:43:08 +0800

    #00

--- app/src/main/java/io/ikws4/weiju/xposed/VariableHook.kt
+++ app/src/main/java/io/ikws4/weiju/xposed/VariableHook.kt
@@ -15,7 +15,7 @@ import io.ikws4.weiju.utilities.XSPUtils
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
-class VariableHook(sp: XSPUtils, private val classLoader: ClassLoader) {
+class VariableHook(private val sp: XSPUtils, private val classLoader: ClassLoader) {
     companion object {
         private const val TAG = "VariableHook"
     }
@@ -26,10 +26,20 @@ class VariableHook(sp: XSPUtils, private val classLoader: ClassLoader) {
     private val model = sp.getString("variable_model")
     private val brand = sp.getString("variable_brand")
     private val release = sp.getString("variable_release")
-    private val longitude = sp.getString("variable_longitude")
-    private val latitude = sp.getString("variable_latitude")
     private val imei = sp.getString("variable_imei")
     private val imsi = sp.getString("variable_imsi")
+
+    private val longitude: Double?
+        get() {
+            return sp.getString("variable_longitude").toDoubleOrNull()
+        }
+    private val latitude: Double?
+        get() {
+            return sp.getString("variable_latitude").toDoubleOrNull()
+        }
+    private var isLocationBaidu: Boolean? = sp.getBoolean("is_location_baidu")
+    private var isLocationTx: Boolean? = sp.getBoolean("is_location_tx")
+    private var isLocationTxFound = false
 
     init {
         if (isEnable) {
@@ -68,74 +78,105 @@ class VariableHook(sp: XSPUtils, private val classLoader: ClassLoader) {
     private fun replaceLocationInfo() {
         Location::class.java.hookMethod("getLongitude") { param ->
             try {
-                param.result =
-                    this@VariableHook.longitude.toDouble()
+                param.result = this@VariableHook.longitude ?: param.result
             } catch (e: Exception) {
                 MainHook.log(e)
             }
         }
         Location::class.java.hookMethod("getLatitude") { param ->
             try {
-                param.result = this@VariableHook.latitude.toDouble()
+                param.result = this@VariableHook.latitude ?: param.result
             } catch (e: Exception) {
                 MainHook.log(e)
             }
         }
 
         try {
-            // 百度
-//            val bdLocation = "com.baidu.location.BDLocation"
-//            bdLocation.hookMethod(classLoader, "getLongitude") { param ->
-//                try {
-//                    param.result =
-//                        this@VariableHook.longitude.toDouble()
-//                } catch (e: Exception) {
-//                    MainHook.log(e)
-//                }
-//            }
-//            bdLocation.hookMethod(classLoader, "getLatitude") { param ->
-//                try {
-//                    param.result =
-//                        this@VariableHook.latitude.toDouble()
-//                } catch (e: Exception) {
-//                    MainHook.log(e)
-//                }
-//            }
+            hookLocationBaidu()
         } catch (e: java.lang.Exception) {
             //ignore
         }
+        try {
+            hookLocationTencent()
+        } catch (e: java.lang.Exception) {
+            //ignore
+        }
+    }
+
+    private fun hookLocationBaidu() {
+        if (isLocationBaidu != true) return
+        // 百度
+        val bdLocation = "com.baidu.location.BDLocation"
+        bdLocation.hookMethod(classLoader, "getLongitude") { param ->
+            try {
+                param.result = this@VariableHook.longitude ?: param.result
+            } catch (e: Exception) {
+                MainHook.log(e)
+            }
+        }
+        bdLocation.hookMethod(classLoader, "getLatitude") { param ->
+            try {
+                param.result = this@VariableHook.latitude ?: param.result
+            } catch (e: Exception) {
+                MainHook.log(e)
+            }
+        }
+        isLocationBaidu = null
+    }
+
+    val locationTxUnhook = HashSet<XC_MethodHook.Unhook>()
+
+    private fun hookLocationTencent() {
+        if (isLocationTx != true) return
+        Log.d(TAG, "hookLocationTencent isLocationTx $isLocationTx")
 
         //腾讯 com.tencent.map.geolocation.TencentLocation
         // 这里查看版本/data/data/com.tencent.mm/files/TencentLocation/conf
         // 6.6.1版本的腾讯地图sdk 实现类为 c.t.m.g.gi
-        Log.d(TAG, "hookDynamicClass: ")
-        hookDynamicClass()
-    }
-
-    fun hookDynamicClass() {
-        XposedBridge.hookAllMethods(DexClassLoader::class.java, "loadClass", object : XC_MethodHook() {
+        val unhook = XposedBridge.hookAllMethods(ClassLoader::class.java, "loadClass", object : XC_MethodHook() {
             @Throws(Throwable::class)
             override fun afterHookedMethod(param: MethodHookParam) {
+                Log.d(TAG, "hookLocationTencent afterHookedMethod $isLocationTxFound")
+                if (isLocationTxFound) return
+                if (param.thisObject !is DexClassLoader) return
                 if (param.hasThrowable()) return
                 if (param.args.size != 1) return
                 val cls = param.result as Class<*>
                 val name = cls.name
                 val classLoader = param.thisObject as ClassLoader
-                if ("c.t.m.g.gi" == name) {     //
-                    Log.d(TAG, "hookDynamicClass: \"c.t.m.g.gi\"")
-                    val txLocation = "c.t.m.g.gi"
-                    txLocation.hookMethod(classLoader, "getLatitude") { param ->
+                var found = false
+                for (type in cls.interfaces) {
+                    if (name == "c.t.m.g.gi") {
+                        Log.d(TAG, "hookLocationTencent: " + type.name)
+                    }
+                    if (type.name == "com.tencent.map.geolocation.TencentLocation") {
+                        found = true
+                        break
+                    }
+                }
+                if (found) {
+                    val it = locationTxUnhook.iterator()
+                    while (it.hasNext()) {
+                        val unhook = it.next();
+                        unhook.unhook()
+                        XposedBridge.unhookMethod(param.method, this)
+                        it.remove()
+                    }
+
+                    isLocationTxFound = true
+                    Log.d(TAG, "hookLocationTencent: \"TencentLocation\" --> $name")
+                    name.hookMethod(classLoader, "getLatitude") { latitudeParam ->
                         try {
-                            param.result = this@VariableHook.latitude.toDouble()
-                            Log.d(TAG, "replaceLocationInfo: " + param.result)
+                            latitudeParam.result = this@VariableHook.latitude ?: latitudeParam.result
+                            Log.d(TAG, "replaceLocationInfo: " + latitudeParam.result)
                         } catch (e: Exception) {
                             MainHook.log(e)
                         }
                     }
-                    txLocation.hookMethod(classLoader, "getLongitude") { param ->
+                    name.hookMethod(classLoader, "getLongitude") { longitudeParam ->
                         try {
-                            param.result = this@VariableHook.longitude.toDouble()
-                            Log.d(TAG, "replaceLocationInfo: " + param.result)
+                            longitudeParam.result = this@VariableHook.longitude ?: longitudeParam.result
+                            Log.d(TAG, "replaceLocationInfo: " + longitudeParam.result)
                         } catch (e: Exception) {
                             MainHook.log(e)
                         }
@@ -143,6 +184,8 @@ class VariableHook(sp: XSPUtils, private val classLoader: ClassLoader) {
                 }
             }
         })
+        Log.d(TAG, "locationTxUnhook: addAll " + unhook?.size)
+        locationTxUnhook.addAll(unhook)
     }
 
     @SuppressLint("MissingPermission")
